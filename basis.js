ddb9d75dfefe263d2ae9a0c1eab74ae54acb2d22
Revathy2000 <revathyr1914@gmail.com>
2023-09-15 23:20:22 +0100

    first commit

---
+++ basis.js
@@ -0,0 +1,89 @@
+// console.log('hai welcome');
+// //strings
+// var firstName='i am js';
+// var t='100';
+// console.log(typeof(t));
+
+// //addition
+// function addition(a,b){
+//     var sum=a+b;
+//     console.log("the result is" +sum);
+// }
+// addition(50,25);
+// //subtraction
+// function subtraction(a,b){
+//     var diff=a-b;
+//     console.log("the result is" +diff);
+// }
+// subtraction(50,25);
+// //multiplication
+// function multiplication(a,b){
+//     var mult=a*b;
+//     console.log("the result is" +mult);
+// }
+// multiplication(5,5);
+// //division
+// function division(a,b){
+//     var div=a/b;
+//     console.log("the result is" +div);
+// }
+// division(130,5);
+
+
+
+//for loop
+// for(let i=0;i<=5;i++){
+//     console.log(i)
+// }
+// for(let i=0;i<5;i++){
+//     console.log(i+1)
+// }
+
+//array elements
+// let fruits=["apple","mango","orange","grapes"]
+// for(let i=0;i<fruits.length;i++){
+//     console.log(fruits[i])
+// }
+
+// let fruits=["apple","mango","orange","grapes"]
+// for(let i in fruits){
+//          console.log(fruits[i])
+// }
+
+// 
+
+
+// var myName="tiya";
+// for(let i of myName){
+//     console.log(i)
+// }
+
+
+//while loop
+// let i=1;
+// while(i<=5){
+//     console.log(i)
+//     i++
+// }
+
+// let fruits=["apple","mango","orange","grapes"]
+// let i=0;
+// while(i<fruits.length){
+//     console.log(fruits[i])
+//     i++
+// }
+
+//do while
+// let i=1;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=5)
+
+
+let fruits = ["apple","mango","orange","grapes"]
+let i = 0;
+do {
+    console.log(fruits[i])
+    i++
+} while (i<fruits.length);
